2d459faa64670391e3a04e49d1691ec1d416e134
unknown <andrequeen123@gmail.com>
2018-02-20 19:21:11 +0000

    Updated 3D Models

---
+++ Assets/tutorialScriptFE.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class tutorialScriptFE : MonoBehaviour, IVirtualButtonEventHandler {
+
+	public GameObject vButtonObject;
+
+	// Use this for initialization
+	void Start () {
+
+		vButtonObject = GameObject.Find("tutorialButton");
+		vButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler(this);
+
+	}
+	
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+		Debug.Log("Button Down!!!!");
+
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
+		Debug.Log("Button Released!!!!");
+//		MeshRenderer targetMeshRenderer = this.GetComponent<MeshRenderer>();
+//		Destroy(targetMeshRenderer);
+//		MeshFilter targetMesh = this.GetComponent<MeshFilter>();
+//		Destroy(targetMesh);
+	}
+}
